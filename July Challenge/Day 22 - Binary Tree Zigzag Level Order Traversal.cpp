83480fbe5eee1e32c161deeacf4d39c56e91e7b2
Wani Sachin Gopal <sachin27071998@gmail.com>
2020-07-22 17:24:57 +0530

    Create Day 22 - Binary Tree Zigzag Level Order Traversal.cpp

---
+++ July Challenge/Day 22 - Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,63 @@
+/*
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root) return res;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        bool zigzag = false;
+        while(!q.empty())
+        {
+            int size = q.size();
+            vector<int> level;
+            
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* curr = q.front();
+                q.pop();
+                level.push_back(curr->val);
+                
+                if(curr->left)
+                    q.push(curr->left);
+                if(curr->right)
+                    q.push(curr->right);
+            }
+            if(zigzag)
+                reverse(level.begin(), level.end());
+            res.push_back(level);
+            zigzag = !zigzag;
+        }
+        return res;
+    }
+};
